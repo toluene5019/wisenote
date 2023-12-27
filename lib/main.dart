38085fd8addd0936a44821002f38301e8442759d
toluene5019 <toluene5019@gmail.com>
2023-12-27 23:51:12 +0200

    logout view (4)

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:wisenote/firebase_options.dart';
 import 'package:wisenote/views/login_view.dart';
 import 'package:wisenote/views/register_view.dart';
 import 'package:wisenote/views/verify_email_view.dart';
-import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,6 +17,7 @@ void main() {
     routes: {
       '/login/': (context) => const LoginView(),
       '/register/': (context) => const RegisterView(),
+      '/notes/': (context) => const NotesView(),
     },
   ));
 }
