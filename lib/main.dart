db6ffc0f17b48e1a6098aea912a4a26a7259e208
toluene5019 <toluene5019@gmail.com>
2024-01-05 21:51:25 +0200

    update notes (17)

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:wisenote/constants/routes.dart';
 import 'package:wisenote/services/auth/auth_service.dart';
 import 'package:wisenote/views/login_view.dart';
-import 'package:wisenote/views/notes/new_note_view.dart';
+import 'package:wisenote/views/notes/create_update_note_view.dart';
 import 'package:wisenote/views/notes/notes_view.dart';
 import 'package:wisenote/views/register_view.dart';
 import 'package:wisenote/views/verify_email_view.dart';
@@ -20,7 +20,7 @@ void main() {
       registerRoute: (context) => const RegisterView(),
       notesRoute: (context) => const NotesView(),
       verifyEmailRoute: (context) => const VerifyEmailView(),
-      newNoteRoute:(context) => const NewNoteView(),
+      createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
     },
   ));
 }
