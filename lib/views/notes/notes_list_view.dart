ed8e559cc14592bef9301b9be81e50950f461f56
toluene5019 <toluene5019@gmail.com>
2024-01-07 00:09:49 +0200

    migrating to firestore service(20)

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:wisenote/services/auth/crud/notes_service.dart';
+import 'package:wisenote/services/cloud/cloud_note.dart';
 import 'package:wisenote/utilities/dialogs/delete_dialog.dart';
 
-typedef NoteCallback = void Function(DatabaseNote note);
+typedef NoteCallback = void Function(CloudNote note);
 
 class NotesListView extends StatelessWidget {
-  final List<DatabaseNote> notes;
+  final Iterable<CloudNote> notes;
+
   final NoteCallback onDeleteNote;
   final NoteCallback onTap;
 
@@ -21,7 +22,7 @@ class NotesListView extends StatelessWidget {
     return ListView.builder(
       itemCount: notes.length,
       itemBuilder: (context, index) {
-        final note = notes[index];
+        final note = notes.elementAt(index);
         return ListTile(
           onTap: () {
             onTap(note);
